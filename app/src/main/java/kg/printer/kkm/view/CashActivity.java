4ab4fd5b983cd0a7ccfa266f120f5609f0708d7e
Ilya Yakubov <i.yakubov.pro@gmail.com>
2023-11-02 20:31:01 +0300

    На экране оплаты добавлены кастомные кнопки ввода цифр

--- app/src/main/java/kg/printer/kkm/view/CashActivity.java
+++ app/src/main/java/kg/printer/kkm/view/CashActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import kg.printer.kkm.R;
@@ -22,8 +23,9 @@ import java.util.Objects;
 public class CashActivity extends UIViewController.BaseAdapter implements View.OnClickListener {
 
     private TextView tvResult, tvChange;
-    private EditText etContributed;
-    private Button btnOk;
+    private EditText etContributed, etNumData;
+    private ImageButton btnClearNum;
+    private Button btnOk, btnEnter, btnZero, btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine;
 
     private double sum = 0;
 
@@ -64,7 +66,20 @@ public class CashActivity extends UIViewController.BaseAdapter implements View.O
         tvResult = findViewById(R.id.tv_result);
         tvChange = findViewById(R.id.tv_change);
         etContributed = findViewById(R.id.et_contributed);
+        etNumData = findViewById(R.id.et_num_data);
         btnOk = findViewById(R.id.btn_ok);
+        btnEnter = findViewById(R.id.btn_enter);
+        btnClearNum = findViewById(R.id.btn_clear_num);
+        btnZero = findViewById(R.id.btn_zero);
+        btnOne = findViewById(R.id.btn_one);
+        btnTwo = findViewById(R.id.btn_two);
+        btnThree = findViewById(R.id.btn_three);
+        btnFour = findViewById(R.id.btn_four);
+        btnFive = findViewById(R.id.btn_five);
+        btnSix = findViewById(R.id.btn_six);
+        btnSeven = findViewById(R.id.btn_seven);
+        btnEight = findViewById(R.id.btn_eight);
+        btnNine = findViewById(R.id.btn_nine);
     }
 
     @Override
@@ -91,6 +106,18 @@ public class CashActivity extends UIViewController.BaseAdapter implements View.O
         });
 
         btnOk.setOnClickListener(this);
+        btnEnter.setOnClickListener(this);
+        btnClearNum.setOnClickListener(this);
+        btnZero.setOnClickListener(this);
+        btnOne.setOnClickListener(this);
+        btnTwo.setOnClickListener(this);
+        btnThree.setOnClickListener(this);
+        btnFour.setOnClickListener(this);
+        btnFive.setOnClickListener(this);
+        btnSix.setOnClickListener(this);
+        btnSeven.setOnClickListener(this);
+        btnEight.setOnClickListener(this);
+        btnNine.setOnClickListener(this);
     }
 
     private void updateView() {
@@ -100,15 +127,59 @@ public class CashActivity extends UIViewController.BaseAdapter implements View.O
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn_ok) {
-            //noinspection EqualsBetweenInconvertibleTypes
-            if (etContributed.getText().equals("")) {
-                return;
-            }
+    public void onClick(View view) {
+        String text = etNumData.getText().toString();
 
-            hideKeyboard(v);
-            finish();
+        switch (view.getId()) {
+            case R.id.btn_ok:
+                //noinspection EqualsBetweenInconvertibleTypes
+                if (etContributed.getText().equals("")) {
+                    return;
+                }
+
+                hideKeyboard(view);
+                finish();
+                break;
+            case R.id.btn_enter:
+                etContributed.setText(text);
+                etNumData.setText("");
+                break;
+            case R.id.btn_zero:
+                etNumData.setText(text + "0");
+                break;
+            case R.id.btn_one:
+                etNumData.setText(text + "1");
+                break;
+            case R.id.btn_two:
+                etNumData.setText(text + "2");
+                break;
+            case R.id.btn_three:
+                etNumData.setText(text + "3");
+                break;
+            case R.id.btn_four:
+                etNumData.setText(text + "4");
+                break;
+            case R.id.btn_five:
+                etNumData.setText(text + "5");
+                break;
+            case R.id.btn_six:
+                etNumData.setText(text + "6");
+                break;
+            case R.id.btn_seven:
+                etNumData.setText(text + "7");
+                break;
+            case R.id.btn_eight:
+                etNumData.setText(text + "8");
+                break;
+            case R.id.btn_nine:
+                etNumData.setText(text + "9");
+                break;
+            case R.id.btn_clear_num:
+                text = text.length() <= 0 ? null : text.substring(0, text.length() - 1);
+                etNumData.setText(text);
+                break;
+            default:
+                break;
         }
     }
 
